d31291d3f5c248263fd674d66da95c527ac5234c
sunwang <sunwang@troila.com>
2018-11-02 15:57:56 +0800

    添加dict信息

--- liaoxuefeng_python3/使用dict和set.py
+++ liaoxuefeng_python3/使用dict和set.py
@@ -3,3 +3,16 @@
 # @Time    : 2018-11-02 15:12
 # @Author  : SunWang
 # @File    : 使用dict和set.py
+
+# dict
+
+d = {'Michael':95 , 'Bob':75 , 'Tracy':85}
+print(d['Michael'])
+d['Adam'] = 67
+print(d['Adam'])
+print(d)
+d['Jack'] = 90
+print(d['Jack'])
+d['Jack'] = 88
+print(d['Jack'])
+print(d['Thomas'])
