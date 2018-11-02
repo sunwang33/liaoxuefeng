e11bfe5d21bd9faa0bf87c9005e14618af597d26
sunwang <sunwang@troila.com>
2018-11-02 14:08:18 +0800

    Initial commit

---
+++ liaoxuefeng_python3/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+# @Time    : 2018-11-02 14:06
+# @Author  : SunWang
+# @File    : __init__.py.py
---
+++ liaoxuefeng_python3/demon1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+# @Time    : 2018-11-02 14:06
+# @Author  : SunWang
+# @File    : demon1.py
+
+print("the first commit,hello github!!!")
